8625b2a25a1f5715035b58e1c7314fe2cbae381d
VManuelPM <v.manuel.nago@gmail.com>
2025-03-10 20:24:14 +0100

    Add test for finding examen by name with empty list scenario

--- app-mockito/src/test/java/com/amoelcodigo/services/ExamenServiceImplTest.java
+++ app-mockito/src/test/java/com/amoelcodigo/services/ExamenServiceImplTest.java
@@ -1,11 +1,13 @@
 package com.amoelcodigo.services;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -26,5 +28,17 @@ public class ExamenServiceImplTest {
     assertNotNull(examen);
     assertEquals(5L, examen.getId());
     assertEquals("Matemáticas", examen.getNombre());
+  }
+
+  @Test
+  void findExamenPorNombreListaVacia() {
+    ExamenRepository repository = mock(ExamenRepository.class);
+    ExamenService service = new ExamenServiceImpl(repository);
+    List<Examen> datos = Collections.emptyList();
+
+    when(repository.findAll()).thenReturn(datos);
+    Optional<Examen> examen = Optional.ofNullable(service.findExamenByName("Matemáticas"));
+
+    assertFalse(examen.isPresent());
   }
 }
